16b6bac00e58faa415f97c3c280bea6f7cd0ec7c
Olexander753 <a862000@gmail.com>
2022-09-06 15:01:08 +0300

    create iternal/db, iternal/schema, pkg/util

---
+++ internal/db/repository.go
@@ -0,0 +1,12 @@
+package db
+
+// import (
+// 	"context"
+// 	"githib.com/"
+// )
+
+// type Repository interface {
+// 	Close()
+// 	InsertModel(ctx context.Context, model schema.Model) error
+// 	// ListModels(ctx context.Context, skip uint64, take uint64) ([]schema.Model, error)
+// }
