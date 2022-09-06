e9a139efbcc9d8144c80459c6d1bcd527ac956a6
Olexander753 <a862000@gmail.com>
2022-09-06 15:11:35 +0300

    commit 1

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/Olexander753/WB_L0.git
+module github.com/Olexander753/WB_L0
 
 go 1.17
--- internal/db/repository.go
+++ internal/db/repository.go
@@ -1,12 +1,13 @@
 package db
 
-// import (
-// 	"context"
-// 	"githib.com/"
-// )
+import (
+	"context"
 
-// type Repository interface {
-// 	Close()
-// 	InsertModel(ctx context.Context, model schema.Model) error
-// 	// ListModels(ctx context.Context, skip uint64, take uint64) ([]schema.Model, error)
-// }
+	"github.com/Olexander753/WB_L0/internal/schema"
+)
+
+type Repository interface {
+	Close()
+	InsertModel(ctx context.Context, model schema.Model) error
+	ListModels(ctx context.Context, skip uint64, take uint64) ([]schema.Model, error)
+}
